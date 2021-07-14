04bfb1b35d6f47e8138d4dc63d32a5989c7da191
Michel Oliveira <michelsantosoliveira@hotmail.com>
2021-07-14 10:49:48 -0300

    Melhora identação

--- scripts.js
+++ scripts.js
@@ -20,13 +20,13 @@ for (let i = 0; i < pixel.length; i += 1) {
 }
 
 function changeColor(event) {
-  for(let i = 0; i < color.length; i += 1) {
-    if(color [i].classList.contains('selected')){
-      color [i].classList.remove('selected')
+  for (let i = 0; i < color.length; i += 1) {
+    if (color[i].classList.contains('selected')) {
+      color[i].classList.remove('selected');
     }
   }
   const colorsSelected = event.target;
- selected.classList.remove('selected');
+  selected.classList.remove('selected');
   colorsSelected.classList.add('selected');
 }
 palette.addEventListener('click', changeColor);
